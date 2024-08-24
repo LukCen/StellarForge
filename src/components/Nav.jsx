89cda55e7deaee3100577a7c57d595c51d4ff8c3
lbjcena <lbjcena@gmail.com>
2024-08-24 23:34:13 +0200

    nav link component expanded - introduced dedicated component for sublists

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,5 @@
 
-import NavItem from "./NavItem"
-
+import NavItem, { NavItemSublist } from "./NavItem"
 
 
 export default function Nav() {
@@ -10,14 +9,14 @@ export default function Nav() {
       <a href="/" className="logo">
         <img src="./page_logo_desktop.webp" alt="Stellar Forge logo" />
       </a>
-      <ul className="menu_list flex gap-4 px-4">
-
-        <NavItem title="Clothes" link="#">
-          <ul className="flex flex-col">
-            <NavItem title="Men" link="#" />
-            <NavItem title="Women" link="#" />
-          </ul>
-        </NavItem>
+      <ul className="menu_list flex relative gap-4 px-4 min-h-[200]">
+        <NavItemSublist
+          title="Clothes"
+          link="#"
+          subitems={
+            [{ title: "Men", link: "#" }, { title: "Women", link: "#" }]
+          }
+        />
 
         <NavItem title="Food" link="#" />
         <NavItem title="Electronics" link="#" />
--- src/components/NavItem.jsx
+++ src/components/NavItem.jsx
@@ -1,18 +1,31 @@
-import { useState } from "react"
+
 import Icons from "./Icons"
 
 const { ChevronRight } = Icons
 
-export default function NavItem({ title, link, children = null }) {
-  const [hover, setHover] = useState(false)
+export default function NavItem({ title, link }) {
+
   return (
-    <li onMouseEnter={() => setHover(true)} onMouseLeave={() => setHover(false)} className="flex relative justify-between bg-grey-light rounded-3xl hover:rounded-none transition-all duration-500 min-h-[30px] items-center">
+    <li className="flex relative justify-between bg-grey-light rounded-3xl hover:rounded-none transition-all duration-250 min-h-[30px] items-center">
       <a className="flex h-full w-full py-1 px-4 items-center transition-transform" href={link}>{title}<ChevronRight /></a>
-      {hover && children && (
-        <ul className="menu_list__submenu flex absolute bottom-0 translate-y-[100%] min-h-fit flex-col h-full w-full px-4 bg-grey-light">
-          {children}
-        </ul>
-      )}
+    </li>
+  )
+}
+
+
+export function NavItemSublist({ title, link, subitems }) {
+  return (
+    <li className="flex relative gap-2 justify-between bg-grey-light rounded-3xl hover:rounded-none transition-all duration-250 min-h-[30px] items-center">
+      <a className="flex h-full w-full py-1 px-4 items-center transition-transform" href={link}>{title}</a>
+      <ul className="sublist flex flex-col gap-2 absolute translate-y-[100%] bottom-[-8px]">
+        {subitems.map((subitem, index) => (
+          <li className="flex relative justify-between bg-grey-light rounded-3xl hover:rounded-none transition-all duration-250 min-h-[30px] items-center" key={index}>
+            <a className="flex h-full w-full py-1 px-4 items-center transition-transform" href={subitem.link}>{subitem.title}</a>
+          </li>
+        ))}
+      </ul>
+
+
     </li>
   )
 }
