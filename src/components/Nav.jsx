a012b9f2ec6f5599fae5bf3bcde62935789d12c9
lbjcena <lbjcena@gmail.com>
2024-09-01 21:02:00 +0200

    product card skeleton finished, added opinions and interactive gallery

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav() {
   return (
     <nav className="min-h-[50px] bg-grey-superlight flex w-full items-center">
       <a href="/" className="logo">
-        <img src="./page_logo_desktop.webp" alt="Stellar Forge logo" />
+        <img src="/page_logo_desktop.webp" alt="Stellar Forge logo" />
       </a>
       <ul className="menu_list flex relative gap-4 px-4 min-h-[200]">
 
--- src/pages/ProductPage.jsx
+++ src/pages/ProductPage.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import Nav from "../components/Nav"
 const apiURL = 'https://dummyjson.com/products/'
 
 export default function ProductPage() {
-  const [isActive, setIsActive] = useState(false)
+  const [isActive, setIsActive] = useState(null)
   let { productId } = useParams()
   const [productParams, setProductParams] = useState(null)
 
@@ -25,17 +26,17 @@ export default function ProductPage() {
   }, [])
 
   return (
-    <div className="product_page flex max-w-[1200px] m-[0_auto]">
+    <div className="product_page flex flex-col max-w-[1200px] m-[0_auto]">
+      <Nav />
       {
         productParams &&
         <>
           <section className="gallery flex gap-5 shadow-lg p-4 rounded-md">
             <div className="subgallery flex flex-col gap-2">
               {productParams?.images.map((e, index) => {
-
                 return (
-                  <button>
-                    <img data-active={isActive} onClick={() => { isActive === false ? setIsActive(true) : setIsActive(false) }} className="subgallery_img shadow-md rounded-lg" data-id={index} key={`0${index}`} src={e} alt="" height={80} width={80} />
+                  <button className="hover:scale-110 transition-transform " onClick={() => setIsActive(index)}>
+                    <img className="subgallery_img shadow-md rounded-lg" data-id={index} key={`0${index}`} src={e} alt="" height={80} width={80} />
                   </button>
                 )
               })}
@@ -46,11 +47,28 @@ export default function ProductPage() {
                 <h2>${productParams?.price}</h2>
               </div>
 
+              <img width={400} height={400} src={isActive !== null ? productParams?.images[isActive] : productParams?.images[1]} alt="" />
             </div>
           </section>
           <section className="details flex gap-5 shadow-lg">
+            <div>
 
-
+            </div>
+          </section>
+          <section className="opinions flex flex-col gap-4">
+            <h2>Opinions</h2>
+            {productParams &&
+              productParams?.reviews.map((e, index) => {
+                return (
+                  <div className="opinion_block flex flex-col shadow-md rounded-md gap-2">
+                    <h3>{e?.reviewerName}</h3>
+                    {/* <span>{e?.date}</span> */}
+                    <span>{`${e?.rating}/5`}</span>
+                    <p>{e?.comment}</p>
+                  </div>
+                )
+              })
+            }
           </section>
         </>
       }
