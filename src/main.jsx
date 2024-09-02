8cb74375cac6cee30f0b909875e58585ed997156
lbjcena <lbjcena@gmail.com>
2024-09-02 19:08:06 +0200

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -20,7 +20,8 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router} />
+    <RouterProvider router={router} >
+      <App />
+    </RouterProvider>
   </StrictMode>
 )
